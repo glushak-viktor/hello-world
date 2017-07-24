b14fc5a064901f65ac52f1c421e9232838adc784
ViktorGlushak <anatema@bk.ru>
2017-07-24 21:59:48 +0500

    нет точек

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,6 @@
-console.log("Hello World!");
-console.log("Hello World!");
-console.log("Hello World!");
+console.log("Hello World!")
+console.log("Hello World!")
+console.log("Hello World!")
 console.log("Hell");
 Print(Hello);
 
